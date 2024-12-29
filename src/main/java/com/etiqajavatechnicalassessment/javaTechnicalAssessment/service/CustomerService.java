b2ac043a484e916ecd530ec83961c98d04832174
JeremyLoh <jjeremyloh1234@gmail.com>
2024-12-29 20:32:45 +0800

    Comment in customer service

--- src/main/java/com/etiqajavatechnicalassessment/javaTechnicalAssessment/service/CustomerService.java
+++ src/main/java/com/etiqajavatechnicalassessment/javaTechnicalAssessment/service/CustomerService.java
@@ -15,19 +15,19 @@ public class CustomerService {
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
-
+    //Get all data
     public List<Customer> getAllCustomers() {
         return customerRepository.findAll();
     }
-
+    //Get specific data by ID
     public Optional<Customer> getCustomerById(String id) {
         return customerRepository.findById(id);
     }
-
+    //Add new customer
     public Customer createCustomer(Customer customer) {
         return customerRepository.save(customer);
     }
-
+    //Update data
     public Customer updateCustomer(String id, Customer customer) {
         if (customerRepository.existsById(id)) {
             customer.setId(id);
@@ -35,7 +35,7 @@ public class CustomerService {
         }
         return null;
     }
-
+    //Delete data
     public void deleteCustomer(String id) {
         customerRepository.deleteById(id);
     }
